1a1a17182269a4acdabae7e34af1bcd178e01391
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-03-02 23:17:18 +0200

    Install node-sass package

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import { Route, Routes, Outlet } from 'react-router-dom';
 import LandingPage from './components/landingPage/LandingPage';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+
 import Home from './components/dashboard/Home';
 import SideBar from './components/dashboard/SideBar';
+
 import Reservations from './components/dashboard/Reservations';
 import DoctorList from './components/dashboard/DoctorList';
 import BookAppointment from './components/dashboard/BookAppointment';
 import DoctorDetail from './components/dashboard/DoctorDetail';
+
+const bodyStyle = {
+  width: '100%',
+  height: '100%',
+  boxSizing: 'border-box',
+  padding: '0',
+  margin: '0',
+  // backgroundColor: '#f5f6fa',
+  background: 'aqua',
+};
 
 const SidebarLayout = () => (
   <>
@@ -18,9 +30,9 @@ const SidebarLayout = () => (
 
 function App() {
   return (
-    <>
+    <div style={bodyStyle}>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route exact path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<SidebarLayout />}>
@@ -31,7 +43,7 @@ function App() {
           <Route path="/user/appointments" element={<BookAppointment />} />
         </Route>
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -3,19 +3,34 @@ import BackgroundSlider from 'react-background-slider';
 import { Link } from 'react-router-dom';
 import image1 from '../../images/landing1.jpg';
 import image2 from '../../images/landing2.jpg';
+import styles from '../scss/LandingPage.module.scss';
 
 const LandingPage = () => (
-  <section className="landing">
+  <section className={styles.landingPage}>
     <BackgroundSlider
       images={[image1, image2]}
       duration={10}
       transition={2}
     />
-    <div className="center">
-      <h1> hello landing</h1>
+    <div className={styles.landing}>
+      <h1>Welcome Back</h1>
+      <h3>Login to your account</h3>
       <Link to="/login" style={{ textDecoration: 'none' }}>
         <button type="button" className="login">Login</button>
       </Link>
+      <div>
+        <nav />
+        <h3>MY DOCTOR</h3>
+        <nav />
+      </div>
+      <p>
+        Don&apos;t have an account?
+        {' '}
+        <Link to="/register" style={{ textDecoration: 'none' }}>
+          Register
+        </Link>
+
+      </p>
     </div>
   </section>
 );
