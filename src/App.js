ad1a4eb9674a2f976753258c11b845a19056445b
ntzwilly <ntzwilly@gmail.com>
2022-03-03 15:48:24 +0200

    Create the navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import SideBar from './components/SideBar/SideBar';
+
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
+      <SideBar />
     </div>
   );
 }
