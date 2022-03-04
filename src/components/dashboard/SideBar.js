12da56bdb029165a5d7f9a5a09639dbb98fa6eaf
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-03-04 08:31:45 +0200

    style navbar mobile menu

--- src/components/dashboard/SideBar.js
+++ src/components/dashboard/SideBar.js
@@ -1,15 +1,27 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+// import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { CgMenu, CgClose } from 'react-icons/cg';
+
+// import { CgMenu, CgClose } from 'react-icons/cg';
 import styles from '../scss/SideBar.module.scss';
 import logo from '../../images/logo_transparent.png';
 
+// const [navbarOpen, setNavbarOpen] = useState(false);
+
+// const handleToggle = () => {
+//   setNavbarOpen((prev) => !prev);
+// };
+
+// const handleToggle = () => {
+//   setNavbarOpen(!navbarOpen);
+// };
+
 const SideBar = () => (
   <nav className={styles['sidebar-dashboard']}>
-    <div>
-      <div><CgMenu /></div>
-      <div><CgClose /></div>
+    {/* <button type="button" onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button> */}
+    <div className={styles.menuIcons}>
+      {/* <div><CgMenu /></div>
+    <div><CgClose /></div> */}
     </div>
     <div className={styles.image}>
       <img src={logo} alt="logo" />
@@ -37,7 +49,10 @@ const SideBar = () => (
       </li>
     </ul>
     <div className="social-icons">
-      <span><i className="fab fa-twitter fa-2x" /></span>
+      <span>
+        <i className="fab fa-twitter fa-2x" />
+        werwrerteryreyryrtyrt
+      </span>
       <span><i className="fab fa-facebook fa-2x" /></span>
       <span><i className="fab fa-instagram fa-2x" /></span>
       <span><i className="fab fa-github fa-2x" /></span>
