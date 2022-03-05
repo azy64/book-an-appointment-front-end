a2f01a7c77d4177265fe6befa0bcfdaf90b86b8c
victor-chukwu <okenwa1993@gmail.com>
2022-03-05 10:09:52 +0100

    Added sidebar toggle

--- src/components/dashboard/SideBar.js
+++ src/components/dashboard/SideBar.js
@@ -1,5 +1,4 @@
-import React from 'react';
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { CgMenu, CgClose } from 'react-icons/cg';
@@ -7,51 +6,51 @@ import { IoLogoTwitter } from 'react-icons/io';
 import { FaLinkedinIn, FaFacebookF, FaPinterestP } from 'react-icons/fa';
 import logo from '../../images/logo_transparent.png';
 import styles from '../scss/SideBar.module.scss';
-// const [navbarOpen, setNavbarOpen] = useState(false);
 
-// const handleToggle = () => {
-//   setNavbarOpen((prev) => !prev);
-// };
-
-const SideBar = () => (
-  <nav className={styles['sidebar-dashboard']}>
-    {/* <button type="button" onClick={handleToggle}>{navbarOpen ? 'Close' : 'Open'}</button> */}
-    <div className={styles.menuIcons}>
-      <div><CgMenu /></div>
-      <div><CgClose /></div>
-    </div>
-    <div className={styles.image}>
-      <img src={logo} alt="logo" />
-    </div>
-    <ul className={styles['sidebar-inner']}>
-      <li className={styles['nav-links']}>
-        <NavLink to="/user/Dashboard" activeClassName={styles.activeLink} exact>
-          Home
-        </NavLink>
-      </li>
-      <li className={styles['nav-links']}>
-        <NavLink to="/user/doctors" activeClassName={styles.activeLink} exact>
-          Available Doctors
-        </NavLink>
-      </li>
-      <li className={styles['nav-links']}>
-        <NavLink to="/user/reservation" activeClassName={styles.activeLink} exact>
-          My Reservations
-        </NavLink>
-      </li>
-      <li className={styles['nav-links']}>
-        <NavLink to="/user/appointments" activeClassName={styles.activeLink} exact>
-          Book Appointment
-        </NavLink>
-      </li>
-    </ul>
-    <ul className={styles['social-icons']}>
-      <li className={styles['social-link']}><IoLogoTwitter /></li>
-      <li className={styles['social-link']}><FaLinkedinIn /></li>
-      <li className={styles['social-link']}><FaFacebookF /></li>
-      <li className={styles['social-link']}><FaPinterestP /></li>
-    </ul>
-  </nav>
-);
+const SideBar = () => {
+  const [navbarShrink, setNavbarShrink] = useState(false);
+  const handleToggle = () => {
+    setNavbarShrink(!navbarShrink);
+  };
+  return (
+    <nav className={styles[`${navbarShrink ? 'shrink-navbar sidebar-dashboard' : 'sidebar-dashboard'}`]}>
+      <button type="button" onClick={handleToggle}>
+        {navbarShrink ? <CgMenu />
+          : <CgClose />}
+      </button>
+      <div className={styles.image}>
+        <img src={logo} alt="logo" />
+      </div>
+      <ul className={styles['sidebar-inner']}>
+        <li className={styles['nav-links']}>
+          <NavLink to="/user/Dashboard" className={styles.activeLink}>
+            Home
+          </NavLink>
+        </li>
+        <li className={styles['nav-links']}>
+          <NavLink to="/user/doctors" className={styles.activeLink}>
+            Available Doctors
+          </NavLink>
+        </li>
+        <li className={styles['nav-links']}>
+          <NavLink to="/user/reservation" className={styles.activeLink}>
+            My Reservations
+          </NavLink>
+        </li>
+        <li className={styles['nav-links']}>
+          <NavLink to="/user/appointments" className={styles.activeLink}>
+            Book Appointment
+          </NavLink>
+        </li>
+      </ul>
+      <ul className={styles['social-icons']}>
+        <li className={styles['social-link']}><IoLogoTwitter /></li>
+        <li className={styles['social-link']}><FaLinkedinIn /></li>
+        <li className={styles['social-link']}><FaFacebookF /></li>
+        <li className={styles['social-link']}><FaPinterestP /></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default SideBar;
