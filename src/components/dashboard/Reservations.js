7cfa0eb9a1b06623f024aa871fccba5aaecd6112
azy64 <allysaidi64@gmail.com>
2022-03-09 11:24:26 +0100

    call a useeffect to dispatch an fetch action

--- src/components/dashboard/Reservations.js
+++ src/components/dashboard/Reservations.js
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAllReservations, singleReservation } from '../../redux/actions/ReservationActions';
 
-const Reservations = () => (
-  <section className="reservations">
-    <h2>Hello Reservations</h2>
-  </section>
-);
+const Reservations = () => {
+  const dispatch = useDispatch();
+  const reservationdatas = useSelector((state) => state.reservationReducer);
+  const { reservations } = reservationdatas;
+  useEffect(() => {
+    dispatch(fetchAllReservations(1));
+  }, []);
 
-export default Reservations;
+  if (!reservations[0]) {
+    return (
+      <h2>Loading...</h2>
+    );
+  }
+};
