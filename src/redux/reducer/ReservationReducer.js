246c767d0359a5744856ade7c94ed79715e6ff03
azy64 <allysaidi64@gmail.com>
2022-03-09 11:07:46 +0100

    Update the file

--- src/redux/reducer/ReservationReducer.js
+++ src/redux/reducer/ReservationReducer.js
@@ -4,3 +4,18 @@ const initiaState = {
   reservations: [],
   reservation: {},
 };
+
+const reservationReducer = (state = initiaState, { type, payload }) => {
+    if (type === Booking.ALL_RESERVATIONS) {
+      return {
+        ...state,
+        reservations: [payload],
+      };
+    }
+    if (type === Booking.ONE_RESERVATION) {
+      return {
+        ...state.reservation, payload,
+      };
+    }
+    return state;
+  };
