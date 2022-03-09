383eb5ffab00e3ff2a8a76be487f3f07e86dd11a
victor-chukwu <okenwa1993@gmail.com>
2022-03-09 14:23:18 +0100

    Add home dashboard

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from '../scss/Home.module.scss';
 
-const Home = () => (
-  <section className={styles['dashboard-home']}>
-    <h2>hello dashboard home</h2>
-  </section>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+  const doctordatas = useSelector((state) => state.doctorReducer);
+  const { doctors } = doctordatas;
+  const firstfive = doctors[0].slice(0,5)
+  if (!firstfive) {
+    return (
+      <h2>Loading</h2>
+    );
+  }
+  return (
+    <section className={styles['doctor-list']}>
+      <h2>List of Doctors</h2>
+      <div className="list">
+        {
+        firstfive.map((item) => (
+          <div className="list-items" key={item.id}>
+            <img src={item.image} style={{ width: '70', height: '70' }} alt={item.name} />
+            <h3>{item.firstName}</h3>
+            <h4>{item.lastName}</h4>
+            <Link to={`${item.id}`} onClick={() => dispatch(singleDoctor(item.id))}>
+              <button type="button">Book Now</button>
+            </Link>
+          </div>
+        ))
+      }
+      </div>
+    </section>
+  );
+  };
 
 export default Home;
