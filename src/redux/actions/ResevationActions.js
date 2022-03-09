34bd25807ce32daaca8dbd1952f4af445a1e79a1
azy64 <allysaidi64@gmail.com>
2022-03-09 11:02:03 +0100

    Add code for fetching reservations

--- src/redux/actions/ResevationActions.js
+++ src/redux/actions/ResevationActions.js
@@ -1,2 +1,11 @@
 import { Booking } from '../constants/Constants';
 import { fetchReservations, ONEReservation } from '../Services';
+
+export const fetchAllReservations = (userid) => (async (dispatch) => {
+    const reservation = await fetchReservations(userid);
+    console.log(reservation, 'payload');
+    dispatch({
+      type: Booking.ALL_RESERVATIONS,
+      payload: reservation,
+    });
+  });
